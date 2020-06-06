cfe4b7d5122a7a70c116eee1f03bd745191d4742
Maria Hanzalah <mariahanzalah@Marias-MacBook-Pro.local>
2020-06-06 16:26:57 +0100

    clean architecture

---
+++ src/Web/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+namespace Web
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class BaseApiContoller : Controller
+    {
+
+    }
+}
